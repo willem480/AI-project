aa6ca2660759898f884e591e9abcebe6030d639d
Yucong He <hyc018018@gmail.com>
2026-03-18 18:41:02 +1100

    initial set up

--- part_a/search/program.py
+++ part_a/search/program.py
@@ -3,7 +3,21 @@
 
 from .core import CellState, Coord, Direction, Action, MoveAction, EatAction, CascadeAction
 from .utils import render_board
+import heapq
 
+def heuristic(state):
+    return 0  # Placeholder, replace with your heuristic calculation
+
+def f_score(state, g_score):
+    return g_score + heuristic(state)
+
+def make_hashable(state : dict[Coord, CellState]):
+    # Convert the state dictionary to a frozenset of items for hashing
+    return tuple((i, j) for i, j in state.items())
+
+def rev_hashable(hashable_state):
+    # Convert the frozenset of items back to a dictionary
+    return dict(hashable_state)
 
 def search(
     board: dict[Coord, CellState]
@@ -28,16 +42,41 @@ def search(
     # codes, set the `ansi` flag to True to print a colour-coded version!
     print(render_board(board, ansi=True))
 
-    # Do some impressive AI stuff here to find the solution...
-    # ...
-    # ... (your solution goes here!)
-    # ...
 
-    # Here we're returning "hardcoded" actions as an example of the expected
-    # output format. Of course, you should instead return the result of your
-    # search algorithm. Remember: if no solution is possible for a given input,
-    # return `None` instead of a list.
+    state_pq = []
+    cost_to_state = {make_hashable(board): 0}
+    parents = {make_hashable(board): None}
+    heapq.heappush(state_pq, (f_score(board, 0), make_hashable(board)))
+    
+
+    while state_pq.count != 0:
+        current_state = heapq.heappop(state_pq)
+        
+    #     current = state in open_set with lowest f_score
+    #     remove current from open_set
+
+    #     if is_goal(current):
+    #         return RECONSTRUCT_PATH(parents, current)
+
+    #     for each (next_state, cost) in GET_NEIGHBORS(current):
+
+    #         tentative_g = g_scores[current] + cost
+
+    #         if next_state not in g_scores
+    #            OR tentative_g < g_scores[next_state]:
+
+    #             g_scores[next_state] = tentative_g
+    #             parents[next_state] = current
+
+    #             f_score = tentative_g + heuristic(next_state)
+    #             insert or update next_state in open_set with f_score
+
+    # return FAILURE   // no solution found
+
+    
+    
+
+    
     return [
-        MoveAction(Coord(3, 3), Direction.Down),
-        EatAction(Coord(4, 3), Direction.Down),
+        CascadeAction(Coord(3, 3), Direction.Down)
     ]
