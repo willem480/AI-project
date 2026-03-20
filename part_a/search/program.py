e23134e77e380d77766cf3f89cfeff6ced3ae3cf
Jiachao Zhang <JiachaoZ@student.unimelb.edu.au>
2026-03-20 12:11:30 +0000

    stackMovement

--- part_a/search/program.py
+++ part_a/search/program.py
@@ -1,23 +1,43 @@
 # COMP30024 Artificial Intelligence, Semester 1 2026
 # Project Part A: Single Player Cascade
 
-from .core import CellState, Coord, Direction, Action, MoveAction, EatAction, CascadeAction, PlayerColor
+from .core import CellState, Coord, Direction, Action, MoveAction, EatAction, CascadeAction
 from .utils import render_board
 import heapq
 
 def heuristic(state):
+    h2 = 0
+    red = [] 
+    blue = []
+    for coord, cellstate in state.items():
+        if(cellstate.color == PlayerColor.Blue): #if color is blue
+            blue.append(coord) #take blue
+
+    for coord, cellstate in state:
+        if(cellstate.color == PlayerColor.Red):
+            red.append(coord)
+
+    if(blue == None):
+        return 0
     
-    return  0
+    if(red == None):
+        return float('inf')
+    
+    #h1:THe number of blue stacks
+    h1 = len(blue)
+    #h2: The sum of cloest distance of blue stack to cloest red stack
+    for node_blue in blue:
+        min = 0
+        for node_red in red:
+           distance = abs(node_blue.r - node_red.r) + abs(node_blue.c - node_red.c)
+           if (min < distance):
+               min = distance  
+        h2 += min
 
-def cascade(state : dict[Coord, CellState], action : CascadeAction):
-    new_state = state.copy()
+    w1 = 0.6
+    w2 = 0.8
 
-    return new_state
-
-def move(state : dict[Coord, CellState], action : MoveAction):
-    new_state = state.copy()
-
-    return new_state
+    return  w1 * h1 + w2 * h2# Placeholder, replace with your heuristic calculation
 
 def f_score(state, g_score):
     return g_score + heuristic(state)
@@ -46,6 +66,77 @@ def get_next_states(state : dict[Coord, CellState]):
     #             result.append((state, CascadeAction(coord, Direction.Down), 1))
                 
     return result
+
+def stack_movement(state, coord, direction):
+    new_state = dict(state)
+    try:
+        target = coord + direction #The red target coord
+    
+    except ValueError:#out of bound
+        return
+    
+
+    #MOVE to empty place
+    if(target not in new_state):
+        new_state[target] = new_state.pop(coord)
+        return MoveAction(coord, direction), new_state
+    
+    #Move to another red stack
+    elif(target in new_state and new_state[target].color == PlayerColor.RED):
+        new_state[target] = CellState
+        (PlayerColor.Red, new_state[target].height + new_state[coord].height)#Pile up
+        new_state.pop(coord)
+
+        return MoveAction(coord, direction), new_state
+    
+    #Eat
+    elif(target in new_state and new_state[target].color == PlayerColor.BLUE):
+        if(new_state[coord].height >= new_state[target].height):
+            new_state[target] = CellState(PlayerColor.RED, new_state[coord].height)#Eat
+            new_state.pop(coord)
+            return EatAction(coord, direction), new_state
+        else:
+            return None, None
+
+    return None, None #Red stack cannot make an movement.
+
+
+def push_stack(newstate, pushcoord, stack, direction):
+    try:
+        next_coord = pushcoord + direction
+    except ValueError:#out of bound
+        return
+
+    if next_coord in newstate: #if there is a stack
+        push_stack(newstate, next_coord, newstate.pop(next_coord), direction)
+    
+    newstate[next_coord] = stack #if empty
+
+
+
+def stack_cascade(state, coord, direction):
+    new_state = dict(state)
+    step = new_state[coord].height #How much a red stack can cascade
+    prev = new_state.pop(coord) #red stack move one rid forward
+    for move in range(1, step + 1):
+        try:
+            target = Coord(coord.r + direction.r * move, 
+                           coord.c + direction.c * move)
+        except ValueError: #out of bound
+            continue #For red stack game continue
+
+        if target in new_state:
+            push_stack(new_state, target, new_state.pop(target), direction)#push engine start
+
+        new_state[target] = CellState(PlayerColor.RED, 1)
+
+        
+    return CascadeAction(coord, direction), new_state
+
+
+    
+
+
 
 def search(
     board: dict[Coord, CellState]
